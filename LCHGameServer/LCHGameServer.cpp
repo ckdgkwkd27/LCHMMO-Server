0fe95169d5cf0c8b339e261618e6577ba2620006
Lee Chang Ha <30414979+ckdgkwkd27@users.noreply.github.com>
2023-06-20 19:17:21 +0900

    0620 Commit

--- LCHGameServer/LCHGameServer.cpp
+++ LCHGameServer/LCHGameServer.cpp
@@ -5,7 +5,7 @@
 
 int main()
 {
-    GIocpServer = new IocpServer(L"127.0.0.1", 7777, 5);
+    GIocpServer = new IocpServer(L"127.0.0.1", 7777, 10000);
     GIocpServer->Initialize();
     GIocpServer->StartAccept();
     GIocpServer->Run();
