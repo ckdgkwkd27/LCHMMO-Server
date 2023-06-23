b3c4c340d454e9bd3f88abd9d2c9f8df82dea906
Lee Chang Ha <30414979+ckdgkwkd27@users.noreply.github.com>
2023-06-23 18:57:08 +0900

    0623

--- LCHDummyClient/ServerSession.cpp
+++ LCHDummyClient/ServerSession.cpp
@@ -1,2 +1,23 @@
 #include "pch.h"
 #include "ServerSession.h"
+
+void ServerSession::OnAccepted()
+{
+}
+
+void ServerSession::OnConnected()
+{
+}
+
+void ServerSession::OnDisconnected()
+{
+}
+
+void ServerSession::OnSend(uint32 len)
+{
+}
+
+uint32 ServerSession::OnRecv(char* buffer, uint32 len)
+{
+	return uint32();
+}
--- LCHDummyClient/ServerSession.h
+++ LCHDummyClient/ServerSession.h
@@ -3,5 +3,11 @@
 
 class ServerSession : public Session
 {
+public:
+	virtual void OnAccepted();
+	virtual void OnConnected();
+	virtual void OnDisconnected();
+	virtual void OnSend(uint32 len);
+	virtual uint32 OnRecv(char* buffer, uint32 len);
 };
 
